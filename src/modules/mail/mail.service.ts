c8375a7a900c1add920edbb79afdf76dfadea779
Gigih Rahmandita <91838816+Gigihrahman@users.noreply.github.com>
2025-04-22 16:04:50 +0700

    feat:add forgot password (#3)

---
+++ src/modules/mail/mail.service.ts
@@ -0,0 +1,36 @@
+import nodemailer, { type Transporter } from "nodemailer";
+import path from "path";
+import fs from "fs/promises";
+import Handlebars from "handlebars";
+import { GMAIL_EMAIL, GMAIL_APP_PASSWORD } from "../../config";
+export class MailService {
+  private transporter: Transporter;
+  private templateDir: string;
+  constructor() {
+    this.transporter = nodemailer.createTransport({
+      service: "gmail",
+      auth: { user: GMAIL_EMAIL, pass: GMAIL_APP_PASSWORD },
+    });
+    this.templateDir = path.resolve(__dirname, "./templates");
+  }
+
+  private renderTemplate = async (templateName: string, context: object) => {
+    const templatePath = path.join(this.templateDir, `${templateName}.hbs`);
+
+    const templateSource = (await fs.readFile(templatePath)).toString();
+
+    const compiledTemplate = Handlebars.compile(templateSource);
+    return compiledTemplate(context);
+  };
+
+  sendEmail = async (
+    to: string,
+    subject: string,
+    templateName: string,
+    context: object
+  ) => {
+    const html = await this.renderTemplate(templateName, context);
+
+    await this.transporter.sendMail({ from: "Blog Ku", to, subject, html });
+  };
+}
