40f9ef49001a959af498f45eb4585664923d8167
Gigih Rahmandita <91838816+Gigihrahman@users.noreply.github.com>
2025-04-24 14:33:07 +0700

    feat:add reset password functionality (#4)

---
+++ src/middlewares/jwt.middleware.ts
@@ -0,0 +1,27 @@
+import { NextFunction, Request, Response } from "express";
+import { TokenExpiredError, verify } from "jsonwebtoken";
+import { ApiError } from "../utils/api-error";
+
+export class JwtMiddleware {
+  verifyToken = (secretKey: string) => {
+    return (req: Request, res: Response, next: NextFunction) => {
+      const token = req.headers.authorization?.split(" ")[1];
+      if (!token) {
+        throw new ApiError("Token not found", 401);
+      }
+
+      verify(token, secretKey, (err, payload) => {
+        if (err) {
+          if (err instanceof TokenExpiredError) {
+            throw new ApiError("Token Expired", 401);
+          } else {
+            throw new ApiError("Invalid token", 401);
+          }
+        }
+
+        res.locals.user = payload;
+        next();
+      });
+    };
+  };
+}
